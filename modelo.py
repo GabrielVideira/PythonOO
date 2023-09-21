2b92f9b70b239d1db9f29a3f327891c2e793c241
Gabriel D'Angelo <gvideira900@gmail.com>
2023-09-21 00:42:02 -0300

    heranca 21.09.23

--- modelo.py
+++ modelo.py
@@ -1,23 +1,24 @@
-class Filme:
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
 
-    def dar_like(self):
-        self.__likes += 1
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        self._nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self._likes = 0
 
     @nome.setter
     def nome(self, novo_nome):
         self.__nome = novo_nome.title()
 
 
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas, likes):
         self.nome = nome
         self.ano = ano
@@ -25,5 +26,10 @@ class Serie:
         self.likes = 0
 
 
-vingadores = Filme()
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 print(vingadores.nome)
+
+atlanta = Serie('atlanta', 2018, 2)
+
+print(
+    f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}')

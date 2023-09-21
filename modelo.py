36f858cb29f252db03f901c15b7ced828bf7ede0
Gabriel D'Angelo <gvideira900@gmail.com>
2023-09-21 15:06:13 -0300

    heranca e polimorfismo

--- modelo.py
+++ modelo.py
@@ -2,34 +2,64 @@ class Programa:
     def __init__(self, nome, ano):
         self._nome = nome.title()
         self.ano = ano
-        self.duracao = duracao
         self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} Likes'
 
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self._likes = 0
 
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes'
 
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas, likes):
-        self.nome = nome
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.likes = 0
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(vingadores.nome)
+vingadores.dar_like()
+print(f'{vingadores.nome} - {vingadores.duracao}: {vingadores.likes}')
+
 
 atlanta = Serie('atlanta', 2018, 2)
+atlanta.dar_like()
+atlanta.dar_like()
+print(f'{atlanta.nome} - {atlanta.temporadas}: {atlanta.likes}')
 
-print(
-    f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}')
+listinha = [atlanta, vingadores]
+
+for programa in listinha:
+    if hasattr(programa, 'duracao'):
+        detalhe = f'{programa.duracao} min'
+    else:
+        detalhe = f'{programa.temporadas} temporadas'
+
+    print(f'Nome: {programa.nome} - {detalhe} - Likes: {programa.likes}')
+
+for programa in listinha:
+    print(programa)

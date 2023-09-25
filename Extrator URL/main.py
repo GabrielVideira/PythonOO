2bca0f2ebd7cbc6eb2cbadbc946262e80d4f3eac
Gabriel D'Angelo <gvideira900@gmail.com>
2023-09-24 21:17:26 -0300

    corrigindo exer

--- Extrator URL/main.py
+++ Extrator URL/main.py
@@ -1,3 +1,31 @@
-url = 'http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
-
+url = "bytebank.com/cambio?moedaOrigem=real"
 print(url)
+
+indice_interrogacao = url.find('?')
+url_base = url[0:indice_interrogacao]
+print(url_base)
+
+url_parametros = url[indice_interrogacao+1:36]
+print(url_parametros)
+
+# -------------------------------------------------------
+
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+
+# Separa base e parâmetros
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
+
+# Busca o valor de um parâmetro
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
